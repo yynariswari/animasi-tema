9325ed1820a8de205e7c4541140356442cd01229
Yuniar Nariswari <yuniarnariswari@gmail.com>
2025-12-17 00:34:49 +0700

    set base for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import linaria from "@linaria/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    linaria({
+      /* default options work for most setups */
+    }),
+  ],
+  base: "/animasi-tema/",
+});
